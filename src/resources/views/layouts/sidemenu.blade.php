4dd7a65dfe749a178265170f4530e4891c3bc97f
Juan Goovaerts <juangoovaerts@gmail.com>
2017-09-10 23:43:54 -0500

    Menu
    Menu

---
+++ src/resources/views/layouts/sidemenu.blade.php
@@ -0,0 +1,29 @@
+
+<div class="page-sidebar-wrapper">
+    <div class="page-sidebar navbar-collapse  ">
+        <ul class="page-sidebar-menu page-header-fixed" data-keep-expanded="false"
+            data-auto-scroll="true" data-slide-speed="200">
+            <li class="heading">
+                <h3 class="uppercase">Menú Principal</h3>
+            </li>
+
+            <li class="nav-item contabilidad">
+                <a href="javascript:;" class="nav-link nav-toggle">
+                    <i class="fa fa-cogs"></i>
+                    <span class="title">CONTABILIDAD</span>
+                    <span class="arrow"></span>
+                </a>
+                <ul class="sub-menu" style="display: none;">
+                    <li class="nav-item balance">
+                        <a href="#" class="nav-link ">
+                            <i class="fa fa-cogs"></i>
+                            <span class="title">Balance</span>
+                        </a>
+                    </li>
+
+                </ul>
+            </li>
+
+        </ul>
+    </div>
+</div>
