15dc0f77dc96b6d19906ebde2ca2db273f791078
Juan Goovaerts <juangoovaerts@gmail.com>
2017-09-18 22:49:54 -0500

    Hashids Funcionando
    Configuración de hashids.

--- src/config/app.php
+++ src/config/app.php
@@ -180,6 +180,9 @@ return [
         //Laravel Collective for forms
         Collective\Html\HtmlServiceProvider::class,
 
+        //Hash Ids
+        Vinkla\Hashids\HashidsServiceProvider::class,
+
     ],
 
     /*
@@ -231,6 +234,7 @@ return [
 
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Hashids' => Vinkla\Hashids\Facades\Hashids::class,
 
     ],
 
